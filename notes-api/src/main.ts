88def55025a97a790469de6b550bbd24e718973f
Prince Pal <princepal9120@gmail.com>
2025-10-09 02:36:41 +0530

    add nest-swaggar-cli for documentation

--- notes-api/src/main.ts
+++ notes-api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
@@ -16,6 +17,15 @@ async function bootstrap() {
     forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
     transform: true, // Automatically transform payloads to DTO instances
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Notes Api Documentation')
+    .setDescription('The Notes API description')
+    .setVersion('1.0')
+    .addTag('Notes')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
